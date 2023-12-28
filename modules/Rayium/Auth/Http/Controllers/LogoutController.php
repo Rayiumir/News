0598693a0771804c0456f74b8d978c5eda5b5909
Ali Salehi <alisalehi13800930@gmail.com>
2023-12-29 00:16:37 +0330

    Add(controller): session invalidate & regenerateToken to logout method in LogoutController

--- modules/Rayium/Auth/Http/Controllers/LogoutController.php
+++ modules/Rayium/Auth/Http/Controllers/LogoutController.php
@@ -3,15 +3,16 @@
 namespace modules\Rayium\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    public function logout()
+    public function __invoke(Request $request)
     {
         Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return to_route('login');
     }
-
-
 }
