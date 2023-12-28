f703677d0052484ffb90af8021c8b2fdcfb8a8e8
Raymond Baghumian <swordart2050@gmail.com>
2023-12-29 03:12:17 +0330

    Bug Fix use Controllers

--- modules/Rayium/Auth/Http/Controllers/LogoutController.php
+++ modules/Rayium/Auth/Http/Controllers/LogoutController.php
@@ -4,14 +4,15 @@ namespace modules\Rayium\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Auth;
+use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    public function logout()
+    public function __invoke(Request $request)
     {
         Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return to_route('login');
     }
-
-
 }
--- modules/Rayium/Auth/Routes/auth_routes.php
+++ modules/Rayium/Auth/Routes/auth_routes.php
@@ -1,39 +1,34 @@
 <?php
 
-
-use modules\Rayium\Auth\Http\Controllers\LoginController;
-use modules\Rayium\Auth\Http\Controllers\LogoutController;
-use modules\Rayium\Auth\Http\Controllers\RegisterController;
-use modules\Rayium\Auth\Http\Controllers\ResetController;
-use modules\Rayium\Auth\Http\Controllers\VerifyController;
+use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Rayium\Auth', 'middleware' => 'web'], function ($router){
 
     // Register User
 
-    $router->get('register', [RegisterController::class, 'index'])->name('auth.register');
-    $router->post('register', [RegisterController::class, 'store'])->name('auth.register.store');
+    $router->get('register', [\modules\Rayium\Auth\Http\Controllers\RegisterController::class, 'index'])->name('auth.register');
+    $router->post('register', [\modules\Rayium\Auth\Http\Controllers\RegisterController::class, 'store'])->name('auth.register.store');
 
     // Login User
 
-    $router->get('login', [LoginController::class, 'index'])->name('login');
+    $router->get('login', [\modules\Rayium\Auth\Http\Controllers\LoginController::class, 'index'])->name('login');
     //$router->get('login', [LoginController::class, 'index'])->name('auth.login');
-    $router->post('login', [LoginController::class, 'store'])->name('auth.login.store');
+    $router->post('login', [\modules\Rayium\Auth\Http\Controllers\LoginController::class, 'store'])->name('auth.login.store');
 
     // Verify Email
 
-    $router->get('verify/email', [VerifyController::class, 'view'])->name('auth.email.verify');
-    $router->get('email/verify/{id}/{hash}', [VerifyController::class, 'verify'])->name('verification.verify')->middleware(['auth', 'signed']);
-    $router->get('email/verify/resend', [VerifyController::class, 'resend'])->name('verify.resend')->middleware(['auth', 'throttle:5,1']);
+    $router->get('verify/email', [\modules\Rayium\Auth\Http\Controllers\VerifyController::class, 'view'])->name('auth.email.verify');
+    $router->get('email/verify/{id}/{hash}', [\modules\Rayium\Auth\Http\Controllers\VerifyController::class, 'verify'])->name('verification.verify')->middleware(['auth', 'signed']);
+    $router->get('email/verify/resend', [\modules\Rayium\Auth\Http\Controllers\VerifyController::class, 'resend'])->name('verify.resend')->middleware(['auth', 'throttle:5,1']);
 
     // Password Reset
 
-    $router->get('password/email', [ResetController::class, 'view'])->name('auth.password.email')->middleware('guest');
-    $router->post('password/send-email', [ResetController::class, 'SendEmail'])->name('auth.password.send.email')->middleware('guest');
-    $router->get('password/reset', [ResetController::class, 'reset'])->name('password.reset')->middleware('guest');
-    $router->post('password/reset', [ResetController::class, 'update'])->name('auth.password.update')->middleware('guest');
+    $router->get('password/email', [\modules\Rayium\Auth\Http\Controllers\ResetController::class, 'view'])->name('auth.password.email')->middleware('guest');
+    $router->post('password/send-email', [\modules\Rayium\Auth\Http\Controllers\ResetController::class, 'SendEmail'])->name('auth.password.send.email')->middleware('guest');
+    $router->get('password/reset', [\modules\Rayium\Auth\Http\Controllers\ResetController::class, 'reset'])->name('password.reset')->middleware('guest');
+    $router->post('password/reset', [\modules\Rayium\Auth\Http\Controllers\ResetController::class, 'update'])->name('auth.password.update')->middleware('guest');
 
     // Logout
 
-    $router->get('logout', [LogoutController::class, 'logout'])->name('auth.logout')->middleware('auth');
+    $router->get('logout', \modules\Rayium\Auth\Http\Controllers\LogoutController::class)->name('auth.logout')->middleware('auth');
 });
