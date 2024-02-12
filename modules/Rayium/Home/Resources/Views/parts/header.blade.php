382a52dd8acd5fb1c4b5e9735a9a0cce1a4e3ecc
Raymond Baghumian <swordart2050@gmail.com>
2024-02-13 02:24:18 +0330

    Bug Fix Categories in Headers

--- modules/Rayium/Home/Resources/Views/parts/header.blade.php
+++ modules/Rayium/Home/Resources/Views/parts/header.blade.php
@@ -10,44 +10,42 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}"><i class="fa-duotone fa-home"></i> </a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}"><i class="fa-duotone fa-home"></i></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">پیوند</a>
                 </li>
+                @foreach($categories as $row)
                 <li class="nav-item dropdown">
-                    @foreach($categories as $row)
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            {{$row->title}}
-                        </a>
-                        <ul class="dropdown-menu">
-                            @foreach($row->subCategories as $rows)
-                                <li><a class="dropdown-item" href="#">{{$rows->title}}</a></li>
-                            @endforeach
-                        </ul>
-                    @endforeach
-
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{$row->title}}
+                    </a>
+                    <ul class="dropdown-menu">
+                        @foreach($row->subCategories as $rows)
+                            <li><a class="dropdown-item" href="#">{{$rows->title}}</a></li>
+                        @endforeach
+                    </ul>
                 </li>
-
+                @endforeach
             </ul>
             <div class="d-flex">
                 @if (Route::has('login'))
-                @auth
-                <div class="btn-group me-3">
-                    <button type="button" class="btn btn-success dropdown-toggle rounded-4" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fa-light fa-list"></i> منو کاربری
-                    </button>
-                    <ul class="dropdown-menu rounded-4">
-                        <li><a class="dropdown-item" href="{{ route('admin.index') }}">میزکار مدیر کل</a></li>
-                        <li><a class="dropdown-item" href="{{ route('auth.logout') }}">خروج</a></li>
-                    </ul>
-                </div>
-                @else
-                <a href="{{ route('login') }}" class="btn btn-primary me-3 rounded-4"><i class="fa-light fa-sign-in"></i> ورود </a>
-                @if (Route::has('register'))
-                <a href="{{ route('auth.register') }}" class="btn btn-primary me-3 rounded-4"><i class="fa-light fa-user-plus"></i> عضویت </a>
-                @endif
-                @endauth
+                    @auth
+                        <div class="btn-group me-3">
+                            <button type="button" class="btn btn-success dropdown-toggle rounded-4" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa-light fa-list"></i> منو کاربری
+                            </button>
+                            <ul class="dropdown-menu rounded-4">
+                                <li><a class="dropdown-item" href="{{ route('admin.index') }}">میزکار مدیر کل</a></li>
+                                <li><a class="dropdown-item" href="{{ route('auth.logout') }}">خروج</a></li>
+                            </ul>
+                        </div>
+                        @else
+                            <a href="{{ route('login') }}" class="btn btn-primary me-3 rounded-4"><i class="fa-light fa-sign-in"></i> ورود </a>
+                        @if (Route::has('register'))
+                             <a href="{{ route('auth.register') }}" class="btn btn-primary me-3 rounded-4"><i class="fa-light fa-user-plus"></i> عضویت </a>
+                        @endif
+                    @endauth
                 @endif
             </div>
         </div>
