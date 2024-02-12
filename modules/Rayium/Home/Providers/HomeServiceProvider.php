267d7f020b592b0ccc71bb9fbbfbec8920b2f7d3
Raymond Baghumian <swordart2050@gmail.com>
2024-02-12 15:53:30 +0330

    Use View Composer For Show Categories in Headers

--- modules/Rayium/Home/Providers/HomeServiceProvider.php
+++ modules/Rayium/Home/Providers/HomeServiceProvider.php
@@ -3,17 +3,23 @@
 namespace modules\Rayium\Home\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use modules\Rayium\Category\Repositories\CategoryRepo;
 
 class HomeServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
         $this->loadRoutesFrom(__DIR__ . '/../Routes/home_route.php');
         $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'Home');
     }
 
-    public function boot()
+    public function boot(): void
     {
-        //
+        view()->composer(['Home::parts.header', 'Home::parts.footer'], static function($view){
+            $categoryRepo = new CategoryRepo;
+            $categories = $categoryRepo->getActiveCategories()->get();
+
+            $view->with(['categories' => $categories]);
+        });
     }
 }
--- modules/Rayium/Home/Resources/Views/parts/header.blade.php
+++ modules/Rayium/Home/Resources/Views/parts/header.blade.php
@@ -10,21 +10,23 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">خانه</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}"><i class="fa-duotone fa-home"></i> </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">پیوند</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        دیگر ...
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    @foreach($categories as $row)
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{$row->title}}
+                        </a>
+                        <ul class="dropdown-menu">
+                            @foreach($row->subCategories as $rows)
+                                <li><a class="dropdown-item" href="#">{{$rows->title}}</a></li>
+                            @endforeach
+                        </ul>
+                    @endforeach
+
                 </li>
 
             </ul>
