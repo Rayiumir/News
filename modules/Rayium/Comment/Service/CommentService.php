933f9c2cb7f17f8baa1a639e19102e1c58403b57
Raymond Baghumian <swordart2050@gmail.com>
2024-03-03 02:57:54 +0330

    Bug Fix CommentService

--- modules/Rayium/Comment/Service/CommentService.php
+++ modules/Rayium/Comment/Service/CommentService.php
@@ -18,7 +18,7 @@ class CommentService
         ]);
     }
 
-    public function setStatus(): string
+    private function setStatus(): string
     {
         if (auth()->user()->hasPermissionTo(Permission::PERMISSION_SUPER_ADMIN))
         {
