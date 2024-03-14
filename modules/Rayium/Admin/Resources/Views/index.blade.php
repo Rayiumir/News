9e20bb677fe877875afd68efa42da4899bc352fe
Raymond Baghumian <swordart2050@gmail.com>
2024-03-15 03:11:43 +0330

    Adding Count Posts - Users - Comments in Page Admin

--- modules/Rayium/Admin/Resources/Views/index.blade.php
+++ modules/Rayium/Admin/Resources/Views/index.blade.php
@@ -1,3 +1,31 @@
 <x-Admin::AdminLayout>
-    تست
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card text-bg-light mb-3 text-center rounded-4">
+                <div class="card-body">
+                    <i class="fa-duotone fa-pen-nib fa-4x"></i>
+                    <h4 class="card-title mt-3">تعداد نوشته ها</h4>
+                    <p class="card-text fw-bold">{{\modules\Rayium\Post\Models\Post::count()}}</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card text-bg-success mb-3 text-center rounded-4">
+                <div class="card-body">
+                    <i class="fa-duotone fa-users fa-4x"></i>
+                    <h4 class="card-title mt-3">تعداد کاربران</h4>
+                    <p class="card-text fw-bold">{{\modules\Rayium\User\Models\User::count()}}</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card text-bg-info mb-3 text-center rounded-4">
+                <div class="card-body">
+                    <i class="fa-duotone fa-comments fa-4x"></i>
+                    <h4 class="card-title mt-3">تعداد نظرات</h4>
+                    <p class="card-text fw-bold">{{\modules\Rayium\Comment\Models\Comment::count()}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-Admin::AdminLayout>
