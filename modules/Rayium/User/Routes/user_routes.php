600479b6cef93ea940a96e202edbd74552ba2515
Raymond Baghumian <swordart2050@gmail.com>
2024-06-01 02:00:46 +0330

    Remove ProfileController

--- modules/Rayium/User/Routes/user_routes.php
+++ modules/Rayium/User/Routes/user_routes.php
@@ -12,8 +12,3 @@ Route::group(['prefix' => 'admin', 'middleware' => 'web'], static function ($rou
     $router->patch('/profile', [\modules\Rayium\User\Http\Controllers\UserController::class, 'updateProfile'])->name('update.profile')->middleware('auth');
 });
 
-//Route::group(['prefix' => 'users', 'middleware' => 'web'], static function ($router) {
-//    $router->get('/profile', [\modules\Rayium\User\Http\Controllers\ProfileController::class, 'index'])->name('profile.index');
-//    $router->post('/profile/store', [\modules\Rayium\User\Http\Controllers\ProfileController::class, 'store'])->name('profile.store');
-//});
-
